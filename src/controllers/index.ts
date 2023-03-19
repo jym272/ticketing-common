281315db956f861d59296a1f709b5e01df78ea67
Jorge Clavijo <jym272@gmail.com>
2023-03-19 04:13:54 -0300

    refactor: controlleres/index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,7 +2,7 @@ import { getHomeController } from '@controllers/home';
 import { requireAuthController, verifyCurrentUserController } from '@controllers/auth';
 
 export const commonController = {
-  getHome: getHomeController,
-  requireAuth: requireAuthController,
+  getHome: getHomeController(),
+  requireAuth: requireAuthController(),
   verifyCurrentUser: verifyCurrentUserController
 };
