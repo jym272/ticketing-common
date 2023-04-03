d74c5e86af3537d93f1472987684e54fac049725
Jorge Clavijo <jym272@gmail.com>
2023-04-03 18:23:46 -0300

    feat: healthz route

--- src/routes/utils/index.ts
+++ src/routes/utils/index.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import { utilsController } from '@controllers/index';
 import { httpStatusCodes, throwError } from '@utils/index';
+import { nc } from '@events/nats';
+
+const { OK, NOT_FOUND, BAD_REQUEST } = httpStatusCodes;
 
 export const utils = Router();
+
+// the instance of nc is unique in the lib, it must be initialized by the api
+utils.get('/api/healthz', (req, res) => {
+  const ncIsClosed = nc ? nc.isClosed() : true;
+  if (ncIsClosed) {
+    return res.status(BAD_REQUEST).send({ status: 'error' });
+  }
+  return res.status(OK).send({ status: 'ok' });
+});
 
 utils.get('/crash-server', utilsController.crashServer);
 utils.get('/health', utilsController.health);
 utils.get('/env', utilsController.env);
 utils.all('*', () => {
-  throwError('Not Found.', httpStatusCodes.NOT_FOUND);
+  throwError('Not Found.', NOT_FOUND);
 });
