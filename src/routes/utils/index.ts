90ebbf678f258db180c10603223b5c6bca594af2
Jorge Clavijo <jym272@gmail.com>
2023-04-03 18:50:34 -0300

    feat: test repeair2healthz route

--- src/routes/utils/index.ts
+++ src/routes/utils/index.ts
@@ -1,20 +1,20 @@
 import { Router } from 'express';
 import { utilsController } from '@controllers/index';
 import { httpStatusCodes, throwError } from '@utils/index';
-// import { nc } from '@events/nats';
+import { nc } from '../../events/nats/nats';
 
-const { NOT_FOUND } = httpStatusCodes;
+const { NOT_FOUND, BAD_REQUEST, OK } = httpStatusCodes;
 
 export const utils = Router();
 
 // the instance of nc is unique in the lib, it must be initialized by the api
-// utils.get('/api/healthz', (req, res) => {
-//   const ncIsClosed = nc ? nc.isClosed() : true;
-//   if (ncIsClosed) {
-//     return res.status(BAD_REQUEST).send({ status: 'error' });
-//   }
-//   return res.status(OK).send({ status: 'ok' });
-// });
+utils.get('/api/healthz', (req, res) => {
+  const ncIsClosed = nc ? nc.isClosed() : true;
+  if (ncIsClosed) {
+    return res.status(BAD_REQUEST).send({ status: 'error' });
+  }
+  return res.status(OK).send({ status: 'ok' });
+});
 
 utils.get('/crash-server', utilsController.crashServer);
 utils.get('/health', utilsController.health);
