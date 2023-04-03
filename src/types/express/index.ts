c1f0318682a662303475302294bebee817d0f533
Jorge Clavijo <jym272@gmail.com>
2023-04-03 17:47:30 -0300

    feat: express imported in types 3

--- src/types/express/index.ts
+++ src/types/express/index.ts
@@ -3,6 +3,7 @@ import { JwtPayloadCustom } from '@custom-types/jwt';
 // export {};
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       currentUser?: JwtPayloadCustom;
