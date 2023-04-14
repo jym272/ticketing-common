593af572f5af1f4111346aebd0feda8efdc0a9e7
Jorge Clavijo <jym272@gmail.com>
2023-04-14 18:59:36 -0300

    feat: logging in pub and sub improved

--- src/events/subscribe.ts
+++ src/events/subscribe.ts
@@ -1,8 +1,9 @@
 import { consumerOpts, JetStreamSubscription, JsMsg } from 'nats';
 import { ConsumerOptsBuilder } from 'nats/lib/nats-base-client/types';
-import { extractStreamName, getDurableName, js, sc, Subjects } from '@events/nats';
-import { log } from '@utils/logs';
+import { extractStreamName, getDurableName, js, Subjects } from '@events/nats';
+import { log, logMessage } from '@utils/logs';
 import { getEnvOrFail } from '@utils/env';
+import chalk from 'chalk';
 
 const getOptsBuilderConfigured = (subj: Subjects, queueGroupName: string): ConsumerOptsBuilder => {
   const opts = consumerOpts();
@@ -23,13 +24,19 @@ export const subscribe = async (subj: Subjects, queueGroupName: string, cb: (m: 
     log(`Error subscribing to ${subj}`, e);
     throw e;
   }
-  log(`[${subj}] subscription opened`);
+  log(chalk`{gray [}{blue ${subj}}{gray ]} {bold.green subscription opened}`);
+
   for await (const m of sub) {
-    log(`[${m.seq}]: [${sub.getProcessed()}]: ${sc.decode(m.data)}`);
-    // igual no puedo esperar nada
+    const seq = m.seq;
+    const strSeq = m.info.streamSequence;
+    const redeliveryCount = m.info.redeliveryCount;
+    const coloredLog = chalk`{bold.green SUB }{gray [}{cyan str_seq=${strSeq}}{gray ]}{gray [}{magenta seq=${seq}}{gray ]}{gray [}{red rdlvery=${redeliveryCount}}{gray ]}: ${logMessage(
+      m.data
+    )}`;
+    log(coloredLog);
     void cb(m);
   }
-  log(`[${subj}] subscription closed`);
+  log(chalk`{gray [}{blue ${subj}}{gray ]} {bold.green subscription closed}`);
 };
 
 /**
--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,6 +1,36 @@
+import chalk from 'chalk';
+import { sc } from '@events/nats';
+
 // eslint-disable-next-line no-console
 export const log = console.log;
 
 export const successConnectionMsg = (msg: string) => log('\x1b[32m%s\x1b[0m', msg);
 //TODO: pensar bien cuando activar logging, si NODE_ENV no es test, los tests fallan por https
 export const activateLogging = () => !(process.env.NODE_ENV === 'test');
+
+const chalkColors = ['blueBright', 'cyanBright', 'magentaBright', 'yellowBright'];
+
+export const colorObject = (obj: Record<never, unknown>, level = 0): string => {
+  const colorIndex = level % chalkColors.length;
+
+  return `{ ${Object.entries(obj).reduce((acc, [key, value], i, arr) => {
+    let coloredValue = value;
+    if (typeof value === 'object' && value !== null) {
+      coloredValue = colorObject(value, level + 1);
+    } else {
+      coloredValue = chalk`{green ${JSON.stringify(value)}}`;
+    }
+    const addBold = level === 0 ? 'bold.' : '';
+    acc += chalk`{${addBold}${chalkColors[colorIndex]} ${key}}: ${coloredValue}`;
+
+    if (i < arr.length - 1) {
+      acc += ', ';
+    }
+    return acc;
+  }, '')} }`;
+};
+export const logMessage = (data: Uint8Array) => {
+  const msgString = sc.decode(data);
+  const msg = JSON.parse(msgString) as Record<string, unknown>;
+  return colorObject(msg);
+};
