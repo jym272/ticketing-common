50437e6c8ea4898d72d72e0f2f7506a9bd1cf7a3
Jorge Clavijo <jym272@gmail.com>
2023-05-12 18:37:24 -0300

    refactor: secrets readed from /run/secrets/api-secrets

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,18 @@
 import * as dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 dotenv.config();
 
 export const getEnvOrFail = (key: string): string => {
   const value = process.env[key];
   if (!value) {
+    try {
+      const filePath = path.join('/run/secrets/api-secrets', key);
+      const data = fs.readFileSync(filePath, 'utf8');
+      if (data.trim()) return data.trim();
+    } catch (err) {
+      //
+    }
     throw new Error(`Missing environment variable: ${key}`);
   }
   return value;
