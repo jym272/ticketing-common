7175399f33feb9d0b1e12bec3fda171d5f0239e8
Jorge Clavijo <jym272@gmail.com>
2023-05-17 04:05:22 -0300

    test: action release fix 2

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const commonController = {
   verifyCurrentUser: authController.verifyCurrentUser,
   errorHandler: utilsController.errorHandler
 };
-// test_line ignore 89121223d23232323wdwd
+// test_line ignore 89121223d23232323wdwwdwdd
 export * as utils from '@utils/index';
 export * as routes from '@routes/index';
 export * as events from '@events/index';
