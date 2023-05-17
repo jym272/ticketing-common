fd437037172adbbe31f40ba782e12894a2ec5c97
Jorge Clavijo <jym272@gmail.com>
2023-05-17 02:52:15 -0300

    feat: test1

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const commonController = {
   verifyCurrentUser: authController.verifyCurrentUser,
   errorHandler: utilsController.errorHandler
 };
-// test_line ignore 88
+// test_line ignore 8912
 export * as utils from '@utils/index';
 export * as routes from '@routes/index';
 export * as events from '@events/index';
