cb06915223cc3182ac4e13b135d36613b1ac3136
Jorge Clavijo <jym272@gmail.com>
2023-05-17 00:17:32 -0300

    test: actions 13

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const commonController = {
   verifyCurrentUser: authController.verifyCurrentUser,
   errorHandler: utilsController.errorHandler
 };
-// test_line ignore 85
+// test_line ignore 86
 export * as utils from '@utils/index';
 export * as routes from '@routes/index';
 export * as events from '@events/index';
