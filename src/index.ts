e8867f4547b948527ed573a68f7744fb471664ee
Jorge Clavijo <jym272@gmail.com>
2023-06-01 14:07:01 -0300

    feat: description in package.json and publish npm

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const commonController = {
   verifyCurrentUser: authController.verifyCurrentUser,
   errorHandler: utilsController.errorHandler
 };
-
+//
 export * as utils from '@utils/index';
 export * as routes from '@routes/index';
 export * as events from '@events/index';
